d0cec676beaf2e7e95e8b3911d00ed957f63b8e9
Sergei Cherkasov <srjchsv@gmail.com>
2023-11-25 13:53:37 +0000

    refactor: wip - .env load in init - moved all env vars into init - db and open ai clients init in init :) - renamed main.py that had routes with handlers to service.py

--- webapps/balsaFe/app/auth.py
+++ webapps/balsaFe/app/auth.py
@@ -1,20 +1,13 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
-from functools import wraps
+from flask import Blueprint, render_template, request, redirect, url_for, session, current_app
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from google.cloud import firestore
-import os
 
 auth = Blueprint("auth", __name__)
-db = firestore.Client()
-# bad gateway
-#db = firestore.client()
-
 
 @auth.route("/login")
 def login():
     print("=== hit login routine ===")
-    return render_template("login.html", client_id=os.environ.get('CLIENT_ID'), login_uri=os.environ.get('LOGIN_URI'))
+    return render_template("login.html", client_id=current_app.client_id, login_uri=current_app.login_uri)
 
 
 @auth.route("/login/callback", methods=["GET","POST"])
@@ -24,13 +17,13 @@ def callback():
     token = request.form.get("credential")
     try:
         # Verify the token
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), os.environ.get('CLIENT_ID'))
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), current_app.client_id)
 
         # Check if the user's email is in the allowed list
         print("==== Attempting to read firestore. ====")
         email = idinfo["email"]
         print("== email:", email)
-        users_ref = db.collection("allowed_users")
+        users_ref = current_app.firestore_client.collection("allowed_users")
         print("== allowed users:", users_ref)
         user_doc = users_ref.document(email).get()
         print("== user_doc:", user_doc)
@@ -41,7 +34,7 @@ def callback():
         if user_doc.exists:
             session["email"] = email
             print("session: ", session)
-            return redirect(url_for("main.index"))
+            return redirect(url_for("service.index"))
         else:
             # User not allowed
             print("No permission to read document?")
@@ -56,13 +49,3 @@ def callback():
 def logout():
     session.pop("email", None)
     return redirect(url_for("auth.login"))
-
-
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if "email" not in session:
-            return redirect(url_for("auth.login"))
-        return f(*args, **kwargs)
-
-    return decorated_function
