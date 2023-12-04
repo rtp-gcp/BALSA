9e02a225ed2728c1b3f51a17595bf1787606bd86
John F. Davis <davisjf@gmail.com>
2023-12-04 19:17:02 +0000

    wip

--- webapps/balsaFe/app/balsa.py
+++ webapps/balsaFe/app/balsa.py
@@ -6,19 +6,20 @@ balsa = Blueprint('balsa', __name__)
 def send_openai_request(prompt, our_mode):
     """Send a request to OpenAI API."""
     system_msg = """
-    When writing code, always obey these rules:
+    When writing code, obey these rules:
 
     * NAME corresponds to a LABEL and is always in column 1.
         - The NAME is at most 8 characters long.
         - The NAME begins with characters A-Z, a-z, $, # or @. 
-    * OPERATION corresponds to an instruction (mnemonic) and it starts in column 16.
-    * OPERANDS corresponds to an instruction argumennts or parameters.
+    * OPERATION corresponds to an instruction (mnemonic) and starts in column 10.
+    * OPERANDS corresponds to instruction argumennts or parameters and starts in column 15.
         - Multiple operands are separated by a comma `,`.
         - Space ` ` characters are not permitted between OPERANDS.
-    * COMMENT a comment has two starting locations.  
+    * COMMENT corresponds to non functional text and has two possible starting locations.  
         - If the entire line is a comment, then the comment marker `*` starts in column 1.
         - If the comment is used at the end of a line of code, it starts at column 32.
-    * Column 72 is used to identify a continuation of the current line to the next
+    * Column 72 is used to identify a continuation of the current line to the next.
+        - Only use a continuation character when an instruction line spans more than 65 columns.
         - In this case, use a `x` in column 72 on the first line.
         - On the second continued line, code starts at column 16.
 
@@ -28,11 +29,9 @@ def send_openai_request(prompt, our_mode):
     code here
     ```
 
-    Above the code section show a ruler which has the column numbers.
-
     Unless explictly told to do so, do not include any commentary.
 
-    When specifiying registers be explicity as in register R1, rather than 1.
+    When specifiying registers be explicit.  For example when referring to register one, use R1 rather than 1.
     """
 
     our_model = current_app.openai_tuned_model if our_mode == "tuned" else current_app.openai_default_model
